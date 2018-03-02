b70039466a9525c5781aefe9630c74c6ce9ca6ee
marcops <marcopstefani@gmail.com>
2018-03-02 08:12:23 -0300

    initial version - proof of concept

---
+++ src/main/java/org/marco/Application.java
@@ -0,0 +1,25 @@
+package org.marco;
+
+import org.marco.service.WebCrawlerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	
+	@Autowired
+	private WebCrawlerService webCrawlerService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		webCrawlerService.getPageLinks("http://www.soscarro.com.br/", 0);
+	}
+	
+	
+}
---
+++ src/main/java/org/marco/service/WebCrawlerService.java
@@ -0,0 +1,39 @@
+package org.marco.service;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebCrawlerService {
+	private static final int MAX_DEPTH = 2;
+	private HashSet<String> links;
+
+	public WebCrawlerService() {
+		links = new HashSet<String>();
+	}
+
+	public void getPageLinks(String URL, int depth) {
+		if ((!links.contains(URL) && (depth < MAX_DEPTH))) {
+			System.out.println(">> Depth: " + depth + " [" + URL + "]");
+			try {
+				links.add(URL);
+
+				Document document = Jsoup.connect(URL).get();
+				Elements linksOnPage = document.select("a[href]");
+
+				depth++;
+				for (Element page : linksOnPage) {
+					getPageLinks(page.attr("abs:href"), depth);
+				}
+			} catch (IOException e) {
+				System.err.println("For '" + URL + "': " + e.getMessage());
+			}
+		}
+	}
+}
